37929bcea102303e1920372d53aa80a466cc0022
jerry <junfeng.shi@going-link.com>
2022-02-23 16:41:29 +0800

    first commit

---
+++ src/main/java/com/zytestsrm/Test/Test.java
@@ -0,0 +1,13 @@
+package com.zytestsrm.Test;
+
+/**
+ * @Author: Jerry_shi34268
+ * @Date: 2022/2/23 16:38
+ * @Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
---
+++ src/main/java/com/zytestsrm/entity/User.java
@@ -0,0 +1,29 @@
+package com.zytestsrm.entity;
+
+/**
+ * @Author: Jerry_shi34268
+ * @Date: 2022/2/23 16:37
+ * @Description:
+ */
+public class User {
+
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private Integer id;
+}
